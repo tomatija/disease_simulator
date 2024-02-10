77fe283bd8eea64f62cc0e8b9992e956919e43cc
tomatija <tomazic.matija@gmail.com>
2024-02-10 18:19:42 +0100

    added infection and recovery

--- agent.py
+++ agent.py
@@ -1,44 +1,70 @@
 import random
 import math
 
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtMultimedia import *
+
 from simulation_window import create_circle
+
 class Agent:
-    def __init__(self, x, y, r, color):
+    def __init__(self, x, y, r, color, width, height):
         self.x = x
         self.y = y
         self.r = r
+        self.width = width
+        self.height = height
         self.circle = create_circle(x, y, r, color)
         self.direction = random.uniform(0, 2 * math.pi)
-        self.speed = 2#random.uniform(1, 5)
+        self.speed = random.uniform(1, 3)
+
+        self.is_infected = False
+        self.infected_time = 0
+        self.infection_radius = 10
+        self.infection_time_limit = random.uniform(300, 500)
+
+        self.direction_change_interval = 5
     
+    def infect(self):
+        self.is_infected = True
+        self.circle.setPen(QPen(QBrush(Qt.red), 1))
+    
+
+    def cure(self):
+        self.is_infected = False
+        self.circle.setPen(QPen(QBrush(Qt.green), 1))
+        self.infected_time = 0
+
+    def is_in_radius(self, other_agent, radius=10):
+        return math.sqrt((self.x - other_agent.x) ** 2 + (self.y - other_agent.y) ** 2) < radius
+
     def calculate_new_position(self):
         new_x = self.x + self.speed * math.cos(self.direction)
         new_y = self.y + self.speed * math.sin(self.direction)
         return new_x, new_y
-        # self.x = new_x
-        # self.y = new_y 
-        # self.circle.setPos(new_x, new_y)
-    
-    def change_direction(self, hit_wall=False):
-        if hit_wall:
-            self.direction = random.uniform(0, 2 * math.pi)
-        else:
-            self.direction = self.direction - math.pi
 
-    def move(self, x, y):
+    def move(self):
+        x, y = self.calculate_new_position()
+        self.check_wall_collision(x, y, self.width, self.height)
         self.x = x
         self.y = y
-        self.direction = self.direction + random.uniform((-math.pi / 9), (math.pi / 9))
+        self.direction_change_interval -= 1
+        self.infected_time += 1
+        if self.direction_change_interval <= 0:
+            self.direction_change_interval = 5
+            self.direction = self.direction + random.uniform((-math.pi / 9), (math.pi / 9))
+        if self.is_infected and self.infected_time > self.infection_time_limit:
+            self.cure()
         self.circle.setPos(x, y)
     
-    def check_wall_collision(self,x , y, width, height):
-        if(x >= width):
+    def check_wall_collision(self, x, y, width, height):
+        if x >= width or x < 0:
             self.direction = math.pi - self.direction
-        elif (x < 0):
-            self.direction = -math.pi - self.direction
-        elif(y >= height):
-            self.direction = 2*math.pi - self.direction
-        elif (y < 0):
-            self.direction = -self.direction
+        if y >= height or y < 0:
+            self.direction = (-self.direction if y < 0 else 2 * math.pi - self.direction)
     
-    # TODO: add check if wall hit 20 times in 5 seconds then reset position, and if out of bounds reset position
+    def reset_position(self, new_x, new_y):
+        self.x, self.y = new_x, new_y
+        self.num_wall_hits = 0
+        self.circle.setPos(self.x, self.y)
--- simulation.py
+++ simulation.py
@@ -7,21 +7,30 @@ a = SimulationManager(1000, 1000)
 
 agents = []
 
-for x in range(10):
+for x in range(200):
     x, y = a.get_random_position_on_window()
-    new_agent = Agent(x, y, 5, 'green')
+    new_agent = Agent(x, y, 5, 'green', a.view.width(), a.view.height())
     a.add_item_to_scene(new_agent.circle)
     agents.append(new_agent)
 
-# def agent_movement_handler():
-#     # check if movement will move out of bounds
-#     # if so, change direction
-#     if test_agent.x < 0 or test_agent.x > a.view.width():
-#         test_agent.direction = math.pi - test_agent.direction
+for x in agents[:10]:
+    x.infect()
+
+def find_agents_close_to_infected_agent(agent, agents):
+    close_agents = []
+    for other_agent in agents:
+        if not other_agent.is_infected and  agent != other_agent and agent.is_in_radius(other_agent,50):
+            close_agents.append(other_agent)
+    return close_agents
+
 while True:
     for agent in agents:
-        new_x, new_y = agent.calculate_new_position()
-        agent.check_wall_collision(new_x, new_y, a.view.width(), a.view.height())
-        agent.move(new_x, new_y)
+        # find agents close to the infected agent
+        if agent.is_infected:
+            for other_agent in find_agents_close_to_infected_agent(agent, agents):
+                if agent != other_agent and agent.is_in_radius(other_agent):
+                    if not other_agent.is_infected:
+                        other_agent.infect()
+        agent.move()
     a.refresh_window()
     a.wait(0.01)
